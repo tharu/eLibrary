c39512a5844ed2ef24a58ab0d1235c4a50671887
tharu <tharucse@gmail.com>
2023-08-26 11:50:53 -0700

    Add files via upload

---
+++ eLibrary2/src/main/java/edu/miu/cs/cs425/demowebapps/eLibrary/ELibrary2Application.java
@@ -0,0 +1,14 @@
+package edu.miu.cs.cs425.demowebapps.eLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ELibrary2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELibrary2Application.class, args);
+	}
+
+}
---
+++ eLibrary2/src/test/java/edu/miu/cs/cs425/demowebapps/eLibrary/ELibrary2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs425.demowebapps.eLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELibrary2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
